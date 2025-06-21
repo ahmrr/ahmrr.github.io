65a6296f54081b61b687db7a7228dfe609cf8cb4
Ahmer Raza <ahhmerrr.dev@gmail.com>
2025-06-21 16:15:12 +0500

    Added SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: "https://ahmerr.com",
+    integrations: [sitemap(), tailwind()],
 });
